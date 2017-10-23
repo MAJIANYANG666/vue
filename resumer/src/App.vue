f6c1a0bf28bc6b5c0c6feb0eaefaab50c2cfee17
MAJIANYANG666 <1297606787@qq.com>
2017-10-23 20:28:11 +0800

    task-8

--- resumer/src/App.vue
+++ resumer/src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div class=page>
-        <header>
-          <Topbar/>
-        </header>
-        <main>
-          <ResumeEditor/>
-          <ResumePreview/>
-        </main>
+    <header>
+      <Topbar/>
+    </header>
+    <main>
+      <ResumeEditor/>
+      <ResumePreview/>
+    </main>
   </div>
 </template>
 
@@ -16,48 +16,63 @@
   import Topbar from './components/Topbar'
   import ResumeEditor from './components/ResumeEditor'
   import ResumePreview from './components/ResumePreview'
+  import icons from './assets/icons'
+  import store from './store/index'
 
-export default {
-  name: 'app',
-  data: function () {
-    return {
-      text: '你好'
+  export default {
+    name: 'app',
+    store,
+    components: {Topbar, ResumeEditor, ResumePreview},
+    created(){
+      document.body.insertAdjacentHTML('afterbegin', icons) //
     }
-  },
-  components: {Topbar, ResumeEditor, ResumePreview}
-}
+  }
 </script>
 
-<style>
-  .page{
-      height: 100vh;
-      display: flex;
-      flex-direction: column;
-      background: #EAEBEC;
+<style rel="stylesheet/scss" lang="scss">
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  .page {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    background: #EAEBEC;
+    >main {
+      flex-grow: 1;
     }
-    .page>main{
-         flex-grow: 1;
-       }
+    >main {
+      min-width: 1024px;
+      max-width: 1440px;
+      margin-top: 16px;
+      margin-bottom: 16px;
+      display: flex;
+      justify-content: space-between;
+      padding: 0 16px;
+      width: 100%; /* 试试不加这句会怎样 */
+      align-self: center;
+    }
+  }
 
-    .page>main{
-         min-width: 1024px;
-         max-width: 1440px;
-         margin-top: 16px;
-         margin-bottom: 16px;
-         display: flex;
-         justify-content: space-between;
-         padding: 0 16px;
-         width: 100%; /* 试试不加这句会怎样 */
-         align-self: center;
-       }
-
-    #resumeEditor{
-         width: 35%;
-         background: #444;
-       }
-    #resumePreview{
-         flex-grow: 1;
-         margin-left: 16px;
-         background: #777;
-       }
+  #resumeEditor{
+    width: 35%;
+    background: #444;
+  }
+  #resumePreview{
+    flex-grow: 1;
+    margin-left: 16px;
+    background: #777;
+  }
+  svg.icon{
+    height: 1em;
+    width: 1em;
+    fill: currentColor;
+    vertical-align: -0.1em;
+    font-size:16px;
+  }
 </style>
